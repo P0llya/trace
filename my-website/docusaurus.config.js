42a69d19436188396c2c23da3e0b90ba4296ce97
Polina <pshcherbakova2004@gmail.com>
2026-05-04 16:22:27 +0300

    docs: добавить документацию Docusaurus

---
+++ my-website/docusaurus.config.js
@@ -0,0 +1,105 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+
+const simplePlantUML = require("@akebifiky/remark-simple-plantuml");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Документация Trace',
+  tagline: 'Платформа для цифровых коллекций',
+  favicon: 'img/favicon.ico',
+  future: {
+    v4: true,
+  },
+  url: 'https://P0llya.github.io',
+  baseUrl: '/trace/',
+  organizationName: 'P0llya',
+  projectName: 'trace',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  plugins: [
+    ['drawio', {}]
+  ],
+  
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: 'docs',
+          editUrl: 'https://github.com/P0llya/trace/edit/main/my-website/',
+          remarkPlugins: [
+            simplePlantUML
+          ],
+          },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+    [
+      'redocusaurus',
+      {
+        specs: [
+          {
+            id: 'trace',
+            spec: 'api_specs/trace_openapi-fin.yaml',
+          },
+        ],
+        theme: {
+          primaryColor: '#1890ff',
+        },
+      }
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+      title: 'Trace',
+      logo: { alt: 'Trace Logo', src: 'img/logo.svg' },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docs',
+          position: 'left',
+          label: 'Документация',
+        },
+        {
+          href: 'https://github.com/P0llya/trace',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Проект',
+            items: [
+              { label: 'GitHub', href: 'https://github.com/P0llya/trace' },
+              { label: 'Автор', href: 'https://github.com/P0llya' },
+            ],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()} Trace. Сделано с Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ my-website/sidebars.js
@@ -0,0 +1,58 @@
+// @ts-check
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+
+ @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  docs: [
+    'intro',
+    {
+      type: 'category',
+      label: 'Концепция продукта',
+      items: ['concept/overview'],
+    },
+    {
+      type: 'category',
+      label: 'Требования',
+      items: ['requirements/functional', 'requirements/non-functional'],
+    },
+    {
+      type: 'category',
+      label: 'Бизнес-процессы',
+      items: ['business-processes/create-collection'],
+    },
+    {
+      type: 'category',
+      label: 'API',
+      items: ['api/overview'],
+    },
+    {
+      type: 'category',
+      label: 'Модель данных',
+      items: ['data-model/erd'],
+    },
+    {
+      type: 'category',
+      label: 'Архитектура',
+      items: ['architecture/async-thumbnails', 'architecture/platformization', 'architecture/storage'],
+    },
+    {
+      type: 'category',
+      label: 'Шаблоны артефактов',
+      items: ['templates/deployment', 'templates/algorithms-capsule', 'templates/monitoring'],
+    },
+  ],
+};
+
+export default sidebars;
